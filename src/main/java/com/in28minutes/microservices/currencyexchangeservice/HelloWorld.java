a2347444aa40415b9887e1092e7855ec6ed878c0
wqy1991529 <qingyuanwang6@gmail.com>
2023-01-09 09:14:22 +0100

    test

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/HelloWorld.java
@@ -3,7 +3,7 @@ package com.in28minutes.microservices.currencyexchangeservice;
 public class HelloWorld {
 
     public String sayHello(String name) {
-
+            //test
         System.out.println("sayHello called with name : " + name);
         return "Hello " + name;
     }
